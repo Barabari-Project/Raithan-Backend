16d35629a48e49699d678d30761ceaa01039d28f
drumil32 <akheniad@gmail.com>
2024-12-25 23:21:17 +0530

    fix: admin routes

--- src/routes/admin.route.ts
+++ src/routes/admin.route.ts
@@ -5,13 +5,13 @@ import { authAdminMiddleware } from "../middlewares/authMiddleware";
 const router = express.Router();
 
 // admin auth routes
-router.post("/admin/auth/login", login);
+router.post("/auth/login", login);
 
 // admin service provider routes
-router.get("/admin/service-providers", authAdminMiddleware, getServiceProviders);
-router.get("/admin/service-providers/pending-verification", authAdminMiddleware, getServiceProvidersPendingVerification);
-router.post("/admin/service-providers/:id/verify", authAdminMiddleware, verifyServiceProvider);
-router.post("/admin/service-providers/:id/reject", authAdminMiddleware, rejectServiceProvider);
-router.get("/admin/service-providers/:id", authAdminMiddleware, getServiceProviderById);
+router.get("/service-providers", authAdminMiddleware, getServiceProviders);
+router.get("/service-providers/pending-verification", authAdminMiddleware, getServiceProvidersPendingVerification);
+router.post("/service-providers/:id/verify", authAdminMiddleware, verifyServiceProvider);
+router.post("/service-providers/:id/reject", authAdminMiddleware, rejectServiceProvider);
+router.get("/service-providers/:id", authAdminMiddleware, getServiceProviderById);
 
 export default router;
